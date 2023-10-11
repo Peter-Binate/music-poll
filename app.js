9d8ad88738c43bbb6171bf50759c7b223e7238ec
Tixerax <peter.binate@gmail.com>
2023-10-11 16:06:12 +0200

    add mongoose connection

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// On se connecte à la base de donnée
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/apinode"); // Avec une installation local de mongodb
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
